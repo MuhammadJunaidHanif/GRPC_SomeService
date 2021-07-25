5075508fbe2529ff63caa52920d473da03930ab6
Juanid Hanif <jhanif@social27.com>
2021-07-25 07:38:56 +0500

    Add project files.

---
+++ SomeService/Protos/SomeServiceImplementation.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Grpc.Core;
+
+namespace SomeService.Protos
+{
+    public class SomeServiceImplementation : SomeServiceApi.SomeServiceApiBase
+    {
+        public override Task<AddNumbersResponse> AddNumbers(AddNumbersRequest request, ServerCallContext context) =>
+            Task.FromResult(
+                new AddNumbersResponse {
+                    Sum = request.NumA + request.NumB
+                }
+            );
+
+        public override Task<AddNumbersFromArrayResponse> AddNumbersFromArray(AddNumbersFromArrayRequest request, ServerCallContext context) =>
+            Task.FromResult(
+                new AddNumbersFromArrayResponse {
+                    Sum = request.Numbers.Sum()
+                }
+            );
+    }
+}
